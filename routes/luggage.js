01687266afda928545281301648b836fd840de56
NWMSU-SN <S543775@nwmissouri.edu>
2021-11-01 23:10:55 -0500

    addmods

--- routes/luggage.js
+++ routes/luggage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("restaurant", { title: "Search Results Restaurant" });
+  res.render("luggage", { title: "Search Results Restaurant" });
 });
 
 module.exports = router;
